3cf629c876e9ec7c8e8fb4a1568d59fdbecffe11
orig0ne1 <origone.me@gmail.com>
2025-05-14 10:18:29 +0300

    misstake

--- finance-control/src/main/java/com/example/finance_control/SQLiteDialect.java
+++
@@ -1,6 +0,0 @@
-package com.example.finance_control;
-
-import org.hibernate.dialect.Dialect;
-
-public class SQLiteDialect extends Dialect {
-}
--- finance-control/src/main/java/com/example/finance_control/repositories/transactions/TransactionsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.finance_control.repositories.transactions;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionsRepository extends JpaRepository<Transaction, Integer> {
-}
